998a28af1b4170aa7880c8a8751b00480a5a3dac
Eusébio Mackenzie Leite <eusebio.leite16@gmail.com>
2024-02-29 17:31:19 -0300

    Made the code more simple, implementing the match case
    I forgot python 3.10 implemented a version of the switch case found in javascript or java, called match case, so i've implemented it to made the code look better

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
 import argparse
 
-def add(x, y):
-    """Addition function."""
-    return x + y
-
-def sub(x, y):
-    """Subtraction function."""
-    return x - y
-
-def div(x, y):
-    """Division function."""
-    if y == 0:
-        raise ValueError("Division by zero is not allowed.")
-    return x / y
-
-def mul(x, y):
-    """Multiplication function."""
-    return x * y
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple python CLI calculator.")
     parser.add_argument('operation', choices=['a', 's', 'd', 'm'], help='Operation to perform (a: addition, s: subtraction, d: division, m: multiplication)')
@@ -26,13 +8,21 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    if args.operation == 'a':
-        result = add(args.x, args.y)
-    elif args.operation == 's':
-        result = sub(args.x, args.y)
-    elif args.operation == 'd':
-        result = div(args.x, args.y)
-    elif args.operation == 'm':
-        result = mul(args.x, args.y)
-
-    print("Result:", result)
+    match args.operation:
+        case 'a':
+            result = args.x + args.y
+            print("Result:", result)
+        case 's':
+            result = args.x - args.y
+            print("Result:", result)
+        case 'd':
+            if args.y == 0 or args.x == 0:
+                print("Division by zero is not allowed.")
+            else:
+                result = args.x / args.y
+                print("Result:", result)
+        case 'm':
+            result = args.x * args.y
+            print("Result:", result)
+        case _  : 
+            print('Operation to perform (a: addition, s: subtraction, d: division, m: multiplication)')
